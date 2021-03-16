4ee7dbb5897503c89ee8a7694f80506c712ebce6
unknown <bartek.hometowski@gmail.com>
2021-03-16 12:28:47 +0100

    Add mobile UI

---
+++ LearningDictionaryUI.Mobile/LearningDictionaryUI.Mobile/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using LearningDictionaryUI.Mobile.Pages;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LearningDictionaryUI.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new WordsPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
